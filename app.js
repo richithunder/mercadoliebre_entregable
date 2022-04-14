e70ca0b9879bfb0164b05e794abc6bb42f30e6bd
return1 <lorieet@gmail.com>
2022-04-14 20:33:56 -0300

    primer commit agregando todo

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en el puerto: " + PORT)
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+//localhost:3000/
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
+});
