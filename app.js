f880aab284298d927ea56c716be0d5e2f91cfddd
return1 <lorieet@gmail.com>
2022-04-15 01:08:37 -0300

    cambios necesarios para deploy en Heroku agregados

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const path = require("path");
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log("Servidor corriendo en el puerto: " + PORT)
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor corriendo en el puerto: " + 3000)
 });
 
 app.use(express.static(path.join(__dirname, "public")));
